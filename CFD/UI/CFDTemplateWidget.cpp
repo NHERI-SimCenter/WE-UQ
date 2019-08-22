cc2dc6b70583c9910bd3a91724d3cbe10b62556c
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2019-08-22 13:20:47 -0700

    RemoteServices deactivated for CFD - Guided

--- CFD/UI/CFDTemplateWidget.cpp
+++ CFD/UI/CFDTemplateWidget.cpp
@@ -13,9 +13,6 @@ CFDTemplateWidget::CFDTemplateWidget(RandomVariablesContainer *theRandomVariable
     initializeUI();
 
     setupConnections();
-
-    //originalUFilePath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "WE-UQ/U.orig";
-    //originalControlDictPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "WE-UQ/controlDict.orig";
 }
 
 bool CFDTemplateWidget::outputAppDataToJSON(QJsonObject &jsonObject)
@@ -84,76 +81,7 @@ void CFDTemplateWidget::selectButtonPushed()
 void CFDTemplateWidget::initializeUI()
 {
     QVBoxLayout* layout = new QVBoxLayout();
-
-    /*
-
-    loginRequiredLabel = new QLabel(tr("Logging into DesignSafe is required to use CFD - Guided."));
-    layout->addWidget(loginRequiredLabel);
-
-    QGroupBox* CFDGroupBox = new QGroupBox("OpenFOAM Parameters", this);
-
-    QGridLayout *parametersLayout = new QGridLayout();
-
-    //QFormLayout* parametersLayout = new QFormLayout();
-
-    caseEditBox = new QLineEdit(this);
-    caseEditBox->setText("agave://designsafe.storage.community/SimCenter/Software/WE_UQ/Examples/SampleBuilding");
-    QHBoxLayout* caseLayout = new QHBoxLayout();
-    caseLayout->addWidget(caseEditBox);
-    caseEditBox->setToolTip(tr("OpenFOAM Remote Case Directory"));
-    caseSelectButton = new QPushButton(tr("Browse"));
-    caseLayout->addWidget(caseSelectButton);
-
-    QLabel *caseLabel = new QLabel("Case", this);
-    parametersLayout->addLayout(caseLayout,0,1);
-    parametersLayout->addWidget(caseLabel,0,0);
-
-    //parametersLayout->addRow("Case", caseLayout);
-
-    solverComboBox = new QComboBox(this);
-    solverComboBox->addItem("pisoFoam");
-    QLabel *solverLabel = new QLabel("Solver", this);
-   // parametersLayout->addRow("Solver", solverComboBox);
-    parametersLayout->addWidget(solverLabel,1,0);
-    parametersLayout->addWidget(solverComboBox,1,1);
-    solverComboBox->setToolTip(tr("OpenFOAM solver used in the analysis"));
-
-    QLabel *forceLabel = new QLabel("Force Calculation", this);
-    QComboBox* forceComboBox = new QComboBox();
-    forceComboBox->addItem("Binning with uniform floor heights");
-    //parametersLayout->addRow("Force Calculation     ", forceComboBox);
-     parametersLayout->addWidget(forceComboBox,2,1);
-     parametersLayout->addWidget(forceLabel,2,0);
-    forceComboBox->setToolTip(tr("Method used for calculating the forces on the building model"));
-
-    QComboBox* meshingComboBox = new QComboBox(this);
-    meshingComboBox->addItem("blockMesh");
-    QLabel *meshingLabel = new QLabel("Meshing");
-    //parametersLayout->addRow("Meshing", meshingComboBox);
-    parametersLayout->addWidget(meshingComboBox,3,1);
-    parametersLayout->addWidget(meshingLabel,3,0);
-
-    meshingComboBox->setToolTip(tr("Method used for generating the mesh for the model"));
-
-    QLabel *startTimeLabel = new QLabel("Start time", this);
-    parametersLayout->addWidget(startTimeLabel, 4, 0);
-    startTimeBox = new QDoubleSpinBox(this);
-    parametersLayout->addWidget(startTimeBox, 4, 1);
-    startTimeBox->setDecimals(3);
-    startTimeBox->setSingleStep(0.001);
-    startTimeBox->setMinimum(0);
-    startTimeBox->setValue(0.01);
-    startTimeBox->setToolTip(tr("The time in the OpenFOAM simulation when the building force event starts. Forces before that time are ignored."));
-
-    //parametersLayout->setMargin();
-    CFDGroupBox->setLayout(parametersLayout);
-
-    */
-
     layout->addWidget(parameterWidget);
-
-    layout->addStretch();
-
     this->setLayout(layout);
     this->setEnabled(true);
 }
@@ -162,6 +90,7 @@ void CFDTemplateWidget::setupConnections()
 {
     connect(caseSelectButton, &QPushButton::clicked, this, &CFDTemplateWidget::selectButtonPushed);
 
+    /*
     connect(remoteService, &RemoteService::loginReturn, this, [this](bool loggedIn)
     {
         if(loggedIn)
@@ -179,5 +108,5 @@ void CFDTemplateWidget::setupConnections()
             this->setDisabled(true);
         }
     });
-
+    */
 }
