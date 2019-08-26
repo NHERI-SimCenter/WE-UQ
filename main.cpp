88ab562d24dad9da3838ac06bc9623e80a136bed
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2019-08-26 16:49:52 -0700

    Fixing style sheet problem on Mac
    * This also activates the stylesheets, as it should
    * cleaning up main()
    * moving the about WE-UQ text into a resource file (standard HTML)

--- main.cpp
+++ main.cpp
@@ -47,151 +47,147 @@ void customMessageOutput(QtMsgType type, const QMessageLogContext &context, cons
         abort();
 }
 
+QString openStyleFiles()
+{
+    QString ret;
+    QFile mainStyleFile(":/Resources/styleSheets/WEUQStyles.qss");
+
+#ifdef Q_OS_WIN
+    QFile appendedStyle(":/Resources/styleSheets/WEUQWin.qss");
+#endif
+
+#ifdef Q_OS_MACOS
+    QFile appendedStyle(":/Resources/styleSheets/WEUQMac.qss");
+#endif
+
+#ifdef Q_OS_LINUX
+    QFile appendedStyle(":/Resources/styleSheets/WEUQLinux.qss");
+#endif
+
+    if (!mainStyleFile.open(QFile::ReadOnly))
+    {
+        return ret;
+    }
+
+    if (!appendedStyle.open(QFile::ReadOnly))
+    {
+        return ret;
+    }
+
+    ret = ret.append(mainStyleFile.readAll());
+    ret = ret.append(appendedStyle.readAll());
+
+    mainStyleFile.close();
+    appendedStyle.close();
+
+    return ret;
+}
+
 
 int main(int argc, char *argv[])
 {
 
-  //
-  //Setting Core Application Name, Organization, Version and Google Analytics Tracking Id
-  //
+    //
+    //Setting Core Application Name, Organization, Version and Google Analytics Tracking Id
+    //
 
-  QCoreApplication::setApplicationName("WE-UQ");
-  QCoreApplication::setOrganizationName("SimCenter");
-  QCoreApplication::setApplicationVersion("1.0.0");
-  // turn off while developing  GoogleAnalytics::SetTrackingId("UA-121615795-1");
-  GoogleAnalytics::StartSession();
-  GoogleAnalytics::ReportStart();
+    QCoreApplication::setApplicationName("WE-UQ");
+    QCoreApplication::setOrganizationName("SimCenter");
+    QCoreApplication::setApplicationVersion("1.0.0");
+    // turn off while developing  GoogleAnalytics::SetTrackingId("UA-121615795-1");
+    GoogleAnalytics::StartSession();
+    GoogleAnalytics::ReportStart();
 
-  //
-  // set up logging of output messages for user debugging
-  //
+    //
+    // set up logging of output messages for user debugging
+    //
 
-  // remove old log file
-  QFile debugFile("debug.log");
-  debugFile.remove();
+    // remove old log file
+    QFile debugFile("debug.log");
+    debugFile.remove();
 
-  QByteArray envVar = qgetenv("QTDIR");       //  check if the app is run in Qt Creator
-  
-  if (envVar.isEmpty())
-    logToFile = true;
-  
-  qInstallMessageHandler(customMessageOutput);
+    QByteArray envVar = qgetenv("QTDIR");       //  check if the app is run in Qt Creator
 
-  // window scaling for mac
-  QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    if (envVar.isEmpty())
+        logToFile = true;
 
-  QApplication a(argc, argv);
+    qInstallMessageHandler(customMessageOutput);
 
-  //
-  // create a remote interface
-  //
+    // window scaling for mac
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-  QString tenant("designsafe");
-  QString storage("agave://designsafe.storage.default/");
-  QString dirName("WE-UQ");
+    QApplication a(argc, argv);
 
-  AgaveCurl *theRemoteService = new AgaveCurl(tenant, storage, &dirName);
+    //
+    // create a remote interface
+    //
 
+    QString tenant("designsafe");
+    QString storage("agave://designsafe.storage.default/");
+    QString dirName("WE-UQ");
 
-  //
-  // create the main window
-  //
-  WorkflowAppWidget *theInputApp = new WorkflowAppWE(theRemoteService);
-  MainWindowWorkflowApp w(QString("WE-UQ: Wind Engineering with Uncertainty Quantification"),theInputApp, theRemoteService);
-
-  QString textAboutWE = "\
-          <p> \
-          This is the Performance Based Engineering (WE) Application.\
-          <p> \
-          The WE Application (WE App) is an open-source software \
-          (https://github.com/NHERI-SimCenter/WE-UQ) that provides researchers a tool to \
-          assess the performance of a building to wind loading. The application \
-          focuses on quantifying building performance through decision variables. Given \
-          that the properties of the buildings and the wind events are not known \
-          exactly, and that the simulation software and the user make simplifying \
-          assumptions in the numerical modeling of the structure, the estimate response of\
-           the structure already exhibits significant variability. Such response can be \
-           estimated using our WE-UQ Application (https://simcenter.designsafe-ci.org/\
-              research-tools/cwe-uq-application/).\
-          <p> \
-          Depending on the type of structural system, the fidelity of the numerical model \
-          and the number of EDP samples required, the response history simulations can be \
-          computationally prohibitively expensive. To overcome this impediment, the user \
-          has the option to perform the response simulations on the Stampede2 \
-          supercomputer. Stampede2 is located at the Texas Advanced Computing Center and \
-          made available to the user through NHERI DesignSafe-CI, the cyberinfrastructure \
-          provider for the distributed NSF funded Natural Hazards in Engineering Research \
-          Infrastructure (NHERI) facility.\
-          <p> \
-          The computations are performed in a workflow application. That is, the numerical\
-           simulations are actually performed by a sequence of different applications. The\
-            WE-UQ backend software runs these various applications for the user, taking the \
-            outputs from some programs and providing them as inputs to others. The design \
-            of the WE-UQ App is such that researchers are able to modify the backend \
-            application to utilize their own application in the workflow computations. \
-            This will ensure researchers are not limited to using the default applications\
-             we provide and will be enthused to provide their own applications for others \
-             to use. \
-          <p>\
-          This is Version 1.0.0 of the tool and as such is limited in scope. Researchers are\
-           encouraged to comment on what additional features and applications they would \
-           like to see in the WE-UQ App. If you want a feature, chances are many of your \
-           colleagues would also benefit from it.\
-          <p>";
-
-     w.setAbout(textAboutWE);
+    AgaveCurl *theRemoteService = new AgaveCurl(tenant, storage, &dirName);
 
 
-  QString version("Version 1.0.0");
-  w.setVersion(version);
+    //
+    // create the main window
+    //
+    WorkflowAppWidget *theInputApp = new WorkflowAppWE(theRemoteService);
+    MainWindowWorkflowApp w(QString("WE-UQ: Wind Engineering with Uncertainty Quantification"),theInputApp, theRemoteService);
 
-  QString citeText(" ");
-  w.setCite(citeText);
+    QFile aboutTXT(":/Resources/docs/textAboutWEUQ.html");
+    aboutTXT.open(QIODevice::ReadOnly);
+    QString textAboutWE = aboutTXT.readAll();
+    aboutTXT.close();
+    w.setAbout(textAboutWE);
 
-  QString manualURL("https://www.designsafe-ci.org/data/browser/public/designsafe.storage.community//SimCenter/Software/WE-UQ");
-  w.setDocumentationURL(manualURL);
+    QString version("Version 1.0.0");
+    w.setVersion(version);
 
-  //
-  // move remote interface to a thread
-  //
+    QString citeText(" ");
+    w.setCite(citeText);
 
-  QThread *thread = new QThread();
-  theRemoteService->moveToThread(thread);
+    QString manualURL("https://www.designsafe-ci.org/data/browser/public/designsafe.storage.community//SimCenter/Software/WE-UQ");
+    w.setDocumentationURL(manualURL);
 
-  QWidget::connect(thread, SIGNAL(finished()), theRemoteService, SLOT(deleteLater()));
-  QWidget::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+    //
+    // move remote interface to a thread
+    //
 
-  thread->start();
+    QThread *thread = new QThread();
+    theRemoteService->moveToThread(thread);
 
-  //
-  // show the main window & start the event loop
-  //
+    QWidget::connect(thread, SIGNAL(finished()), theRemoteService, SLOT(deleteLater()));
+    QWidget::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
 
-  w.show();
+    thread->start();
 
-  QFile file(":/styleCommon/style.qss");
-  if(file.open(QFile::ReadOnly)) {
-    QString styleSheet = QLatin1String(file.readAll());
-    a.setStyleSheet(styleSheet);
-  }
+    //
+    // show the main window & start the event loop
+    //
 
+    w.show();
 
-  theInputApp->setStyleSheet("QComboBox {background: #FFFFFF;} \
-QGroupBox {font-weight: bold;}\
-QLineEdit {background-color: #FFFFFF; border: 2px solid darkgray;} \
-QTabWidget::pane {background-color: #ECECEC; border: 1px solid rgb(244, 244, 244);}");
+    /*
+    theInputApp->setStyleSheet("QComboBox {background: #E0E0E0;} \
+                               QGroupBox {font-weight: bold;}\
+                               QLineEdit {background-color: #E0E0E0; border: 2px solid darkgray;} \
+                               QTabWidget::pane {background-color: #ECECEC; border: 1px solid rgb(244, 244, 244);}");
+    */
 
-  int res = a.exec();
+    a.setStyleSheet(openStyleFiles());
 
-  //
-  // on done with event loop, logout & stop the thread
-  //
+    int res = a.exec();
 
-  theRemoteService->logout();
-  thread->quit();
+    //
+    // on done with event loop, logout & stop the thread
+    //
 
-  GoogleAnalytics::EndSession();
+    theRemoteService->logout();
+    thread->quit();
 
-  // done
-  return res;
+    GoogleAnalytics::EndSession();
+
+    // done
+    return res;
 }
