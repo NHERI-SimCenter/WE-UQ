ede3ba58bf62ede637d88c59b8ec1f258d6a6719
Wael Elhaddad <elhaddad@berkeley.edu>
2019-08-30 11:28:13 -0700

    Support for subdomains

---
+++ CFD/UI/SubdomainsModel.h
@@ -0,0 +1,48 @@
+#ifndef SUBDOMAINSMODEL_H
+#define SUBDOMAINSMODEL_H
+
+#include <QObject>
+#include <QAbstractTableModel>
+
+struct Subdomain
+{
+    double inlet = 0.0;
+    double outlet = 0.0;
+    double outward = 0.0;
+    double inward = 0.0;
+    double bottom = 0.0;
+    double top = 0.0;
+    double meshSize = 0.0;
+};
+
+class SubdomainsModel : public QAbstractTableModel
+{
+    Q_OBJECT
+public:
+    explicit SubdomainsModel(int size, QObject *parent = nullptr);
+
+signals:
+
+public slots:
+
+    // QAbstractItemModel interface
+public:
+    int rowCount(const QModelIndex &parent) const override;
+    int columnCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QVector<Subdomain> getSubdomains();
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+    void setSubdomains(int n, double inlet, double outlet,
+                       double outward, double inward,
+                       double bottom, double top,
+                       double innerMeshSize, double outerMeshsize);
+
+private:
+    QVector<Subdomain> subdomains;
+    double interpolate(double start, double end, double factor);
+};
+
+#endif // SUBDOMAINSMODEL_H
