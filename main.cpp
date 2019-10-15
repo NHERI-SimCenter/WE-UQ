79e7598751ec52c4be7c3774330494b662034c22
fmckenna <fmckenna@berkeley.edu>
2019-10-15 10:34:03 -0700

    fmk - updating version to 2.0.1

--- main.cpp
+++ main.cpp
@@ -60,8 +60,8 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setApplicationName("WE-UQ");
     QCoreApplication::setOrganizationName("SimCenter");
-    QCoreApplication::setApplicationVersion("2.0.0");
-    // turn off while developing  GoogleAnalytics::SetTrackingId("UA-121615795-1");
+    QCoreApplication::setApplicationVersion("2.0.1");
+    //    GoogleAnalytics::SetTrackingId("UA-121615795-1");
     GoogleAnalytics::StartSession();
     GoogleAnalytics::ReportStart();
 
@@ -118,7 +118,7 @@ int main(int argc, char *argv[])
     aboutTXT.close();
     w.setAbout(textAboutWE);
 
-    QString version("Version 2.0.0");
+    QString version("Version 2.0.1");
     w.setVersion(version);
 
     QString citeText("Frank McKenna, Peter Mackenzie-Helnwein, Wael Elhaddad, Michael Gardner, Jiawei Wan, & Dae Kun Kwon. (2019, September 30). NHERI-SimCenter/WE-UQ: Version 2.0.0 (Version v2.0.0). Zenodo. http://doi.org/10.5281/zenodo.3464692");
