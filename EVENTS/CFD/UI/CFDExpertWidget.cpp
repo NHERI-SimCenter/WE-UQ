ff75d1faf6c6348526c274c7a271160f9948302d
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2020-07-07 10:16:08 -0700

    making a little bit more space

--- EVENTS/CFD/UI/CFDExpertWidget.cpp
+++ EVENTS/CFD/UI/CFDExpertWidget.cpp
@@ -303,7 +303,6 @@ void CFDExpertWidget::setupConnections()
         if(state)
         {
             inflowWidget->setHidden(false);
-            /* we already downloaded remote files when logging in ... do we need this next line? */
             downloadRemoteCaseFiles();
         }
         else
