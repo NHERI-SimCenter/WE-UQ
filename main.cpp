e01fff8546313a11ab340f75cbbe1d4fb016e7c0
fmckenna <fmckenna@berkeley.edu>
2023-03-07 10:37:19 -0800

    fmk - setting version to 2.2.0

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 
     QCoreApplication::setApplicationName("WE-UQ");
     QCoreApplication::setOrganizationName("SimCenter");
-    QCoreApplication::setApplicationVersion("2.3.0");
+    QCoreApplication::setApplicationVersion("2.2.0");
     // GoogleAnalytics::SetTrackingId("UA-121615795-1");
     GoogleAnalytics::StartSession();
     GoogleAnalytics::ReportStart();
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
     QString aboutSource = ":/Resources/docs/textAboutWEUQ.html";  // this is an HTML file stored under resources
     w.setAbout(aboutTitle,aboutSource);
 
-    QString version("Version 2.3.0");
+    QString version = QString("Version ") + QCoreApplication::applicationVersion();
     w.setVersion(version);
 
     QString citeText("1) Frank McKenna, Peter Mackenzie-Helnwein, Jiawei Wan & Stevan Gavrilovic. (2022, Oct ). NHERI-SimCenter/WE-UQ: Version 2.3.0 (Version v2.3.0). Zenodo. https://doi.org/10.5281/zenodo.3274227 \n\n2) Gregory G. Deierlein, Frank McKenna, Adam Zsarnóczay, Tracy Kijewski-Correa, Ahsan Kareem, Wael Elhaddad, Laura Lowes, Matt J. Schoettler, and Sanjay Govindjee (2020) A Cloud-Enabled Application Framework for Simulating Regional-Scale Impacts of Natural Hazards on the Built Environment. Frontiers in the Built Environment. 6:558706. doi: 10.3389/fbuil.2020.558706");
